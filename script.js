bdaf84561c9b863d4127c26168a1edd75d1fdb5e
iamsid <developer.sid@outlook.com>
2022-06-11 11:15:46 +0300

    handle mobile menu toggle when hamburger menu and menu links are clicked

---
+++ script.js
@@ -0,0 +1,19 @@
+const mobileMenu = document.getElementById('mobile-menu')
+const hamburgerMenu = document.getElementById('hamburger-menu')
+const menuClose = document.getElementById('menu-close')
+const menuLinks = document.querySelectorAll('.menu-links')
+
+
+const toggleMobileMenu = (el) => {
+  el.addEventListener('click', () => {
+    mobileMenu.classList.toggle('display-none')
+
+  })
+}
+
+toggleMobileMenu(hamburgerMenu)
+toggleMobileMenu(menuClose)
+
+menuLinks.forEach(link => {
+  toggleMobileMenu(link)
+})
