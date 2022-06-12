d9faca11a85a4cecce6197d3eacf0d33907ca7ac
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-06-12 15:19:19 +0200

    fix linter errors

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hamburgerMenu = document.getElementById('hamburger-menu');
 const menuClose = document.getElementById('menu-close');
 const menuLinks = document.querySelectorAll('.menu-links');
 const body = document.querySelector('body');
-const header = document.querySelector('header')
+const header = document.querySelector('header');
 
 const toggleMobileMenu = (el) => {
   el.addEventListener('click', () => {
