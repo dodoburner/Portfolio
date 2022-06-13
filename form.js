5ba4c50e9d54c981d8da36d4216c1968cc3ae087
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-06-13 18:31:30 +0200

    finish styling for the validation error

--- form.js
+++ form.js
@@ -1,18 +1,17 @@
 const form = document.getElementById('form');
-const buttonWrapper =  document.getElementById('button-wrapper');
+const buttonWrapper = document.getElementById('button-wrapper');
 const emailValue = document.getElementById('email');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   if (emailValue.value !== emailValue.value.toLowerCase()) {
-  console.log('value', emailValue.value)
-   const errorMessage = document.createElement('p');
-   errorMessage.innerText = 'Please submit your email in lowercase letters!'
-   buttonWrapper.appendChild(errorMessage);
-   errorMessage.classList.add('error-message')
-  } 
-  else {
-    form.submit()
+    if (!document.querySelector('.error-message')) {
+      const errorMessage = document.createElement('p');
+      errorMessage.innerText = 'Please submit your email in lowercase letters!';
+      buttonWrapper.appendChild(errorMessage);
+      errorMessage.classList.add('error-message');
+    }
+  } else {
+    form.submit();
   }
-
-})
+});
