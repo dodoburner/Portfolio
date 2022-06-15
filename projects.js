b377a5aab416ac91ea3f0c4e05bdd308f211ed09
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-06-15 23:01:26 +0200

    fix popup window sizing bug

--- projects.js
+++ projects.js
@@ -57,7 +57,7 @@ mainSection.insertBefore(workSection, mainSection.children[1]);
 projects.forEach((project, index) => {
   const projectHTML = document.createElement('div');
   projectHTML.innerHTML = (`
-  <div class="project-section project1">
+  <div class="project-section">
     <img class="project-img" ${project.mobileImage}>
     <img class="project-img-desktop img${index + 1}" ${project.desktopImage}>
     
