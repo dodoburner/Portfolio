0f8440eab829067f3aecb512eb45a685ccbc917a
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-06-16 23:27:40 +0200

    add buttons to the popup and the styling

--- projects.js
+++ projects.js
@@ -143,6 +143,16 @@ function createPopup(position) {
             </div>
           </div>
         </div>
+        <div class="projects-navigation-buttons">
+            <button type="button">
+            <svg width="20" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M20 .755l-14.374 11.245 14.374 11.219-.619.781-15.381-12 15.391-12 .609.755z"/></svg>
+              previous project
+            </button>
+            <button type="button">
+              next project
+              <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M4 .755l14.374 11.245-14.374 11.219.619.781 15.381-12-15.391-12-.609.755z"/></svg>
+            </button>
+          </div>  
       </div>
   `);
   popup.classList.add('popup');
